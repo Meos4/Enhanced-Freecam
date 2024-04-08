c4edcd0f8a99ce05f53f3a65faa3cae7eaf7d6f5
Meos <meos.pro@gmail.com>
2024-04-08 20:00:03 +0200

    Improves UI

--- src/Common/Debug/Ui.cpp
+++ src/Common/Debug/Ui.cpp
@@ -26,6 +26,7 @@ namespace Debug::Ui
 			u32 size{};
 			std::vector<Call> call;
 			std::size_t lastHovered{ std::size_t(-1) };
+			s32 column{ 3 };
 		} data;
 
 		auto clearCall = [&]()
@@ -41,25 +42,35 @@ namespace Debug::Ui
 		};
 
 		ImGui::Begin("Mips Call", isOpen);
-		ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
-
 		const auto ramSize{ static_cast<u32>(ram.size()) };
+		const auto buttonOneLetterSize{ ::Ui::buttonOneLetterSize() };
 
-		::Ui::drag("Begin", &data.begin, u32(4), "%X", 0, u32(0), ramSize);
-		data.begin = std::clamp(data.begin - (data.begin % 4), u32(0), ramSize);
-		ImGui::PopItemWidth();
+		if (::Ui::button("+", buttonOneLetterSize))
+		{
+			data.begin = std::clamp(data.begin + data.size, u32(0), ramSize);
+		}
 
 		ImGui::SameLine();
-		::Ui::drag("Size", &data.size, u32(4), "%X", 0, u32(0), ramSize);
+		if (::Ui::button("-", buttonOneLetterSize))
+		{
+			data.begin = std::clamp(data.begin - data.size, u32(0), ramSize);
+		}
+
+		ImGui::SameLine();
+		ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x * 0.6f);
+		::Ui::drag("##MCBegin", &data.begin, u32(4), "%X", 0, u32(0), ramSize);
+		data.begin = std::clamp(data.begin - (data.begin % 4), u32(0), ramSize);
+
+		ImGui::SameLine();
+		ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x);
+		::Ui::drag("##MCSize", &data.size, u32(4), "%X", 0, u32(0), ramSize);
 		data.size = std::clamp(data.size - (data.size % 4), u32(0), ramSize - data.begin);
-		ImGui::PopItemWidth();
 
 		auto isACallback = [](Mips_t instr)
 		{
 			return (instr >> 16) % 32 == 0 && (instr & 0x0000FFFF) == 0x0000F809;
 		};
 
-		ImGui::SameLine();
 		if (::Ui::button("Search"))
 		{
 			clearCall();
@@ -83,12 +94,11 @@ namespace Debug::Ui
 
 		if (data.call.size())
 		{
-			ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
+			ImGui::SameLine();
 			if (::Ui::button("Clear"))
 			{
 				clearCall();
 			}
-			ImGui::PopItemWidth();
 
 			auto enableButton = [&](const char* label, bool enable, u32 color)
 			{
@@ -102,48 +112,66 @@ namespace Debug::Ui
 					}
 				}
 				ImGui::PopStyleColor();
-				ImGui::PopItemWidth();
 			};
 
 			enableButton("ON", true, ::Ui::color(::Ui::Color::GreenY));
 			enableButton("OFF", false, ::Ui::color(::Ui::Color::RedX));
+			ImGui::SameLine();
+			ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x);
+			::Ui::slider("##MCColumn", &data.column, "%d", ImGuiSliderFlags_AlwaysClamp, 1, 6);
 
 			bool isOneHovered{};
 			const bool isRMouseDown{ ImGui::GetIO().MouseDown[1] };
+			const auto callButtonSize{ ImGui::CalcTextSize("AAAAAAAA -> AAAAAAAA") };
+			const auto callSize{ data.call.size() };
 
-			for (std::size_t i{}; i < data.call.size(); ++i)
+			auto* const style{ &ImGui::GetStyle() };
+			const auto oldButtonTextAlign{ style->ButtonTextAlign };
+			style->ButtonTextAlign = { 0.f, 0.5f };
+
+			for (std::size_t i{}; i < callSize; i += data.column)
 			{
-				auto* const c{ &data.call[i] };
-				std::string formattedOffset;
+				for (s32 j{}; j < data.column && (i + j) < callSize; ++j)
+				{
+					auto* const c{ &data.call[i + j] };
+					std::string formattedOffset;
 
-				if (isACallback(data.call[i].instr))
-				{
-					formattedOffset = std::format("{:08X} -> Callback", c->offset);
-				}
-				else
-				{
-					formattedOffset = std::format("{:08X} -> {:08X}", c->offset, (c->instr & ~0x0C000000) << 2);
-				}
-
-				::Ui::pushStyleColor(c->isEnabled ? ::Ui::color(::Ui::Color::GreenY) : ::Ui::color(::Ui::Color::RedX), ImGuiCol_Button, ImGuiCol_ButtonHovered, ImGuiCol_ButtonActive);
-				if (::Ui::button(formattedOffset.c_str()))
-				{
-					c->isEnabled = !c->isEnabled;
-				}
-				ImGui::PopStyleColor(3);
-
-				if (ImGui::IsItemHovered())
-				{
-					isOneHovered = true;
-					if (isRMouseDown && data.lastHovered != i)
+					if (isACallback(data.call[i].instr))
 					{
-						data.lastHovered = i;
+						formattedOffset = std::format("{:08X} -> Callback", c->offset);
+					}
+					else
+					{
+						formattedOffset = std::format("{:08X} -> {:08X}", c->offset, (c->instr & ~0x0C000000) << 2);
+					}
+
+					::Ui::pushStyleColor(c->isEnabled ? ::Ui::color(::Ui::Color::GreenY) : ::Ui::color(::Ui::Color::RedX), ImGuiCol_Button, ImGuiCol_ButtonHovered, ImGuiCol_ButtonActive);
+					if (::Ui::button(formattedOffset.c_str(), { callButtonSize.x, 0.f }))
+					{
 						c->isEnabled = !c->isEnabled;
 					}
-				}
+					ImGui::PopStyleColor(3);
 
-				ram.write(c->offset, c->isEnabled ? c->instr : 0x00000000);
+					if (ImGui::IsItemHovered())
+					{
+						isOneHovered = true;
+						if (isRMouseDown && data.lastHovered != i)
+						{
+							data.lastHovered = i;
+							c->isEnabled = !c->isEnabled;
+						}
+					}
+
+					ram.write(c->offset, c->isEnabled ? c->instr : 0x00000000);
+
+					if (j != data.column - 1 && (i + j) < callSize - 1)
+					{
+						ImGui::SameLine();
+					}
+				}
 			}
+
+			style->ButtonTextAlign = oldButtonTextAlign;
 
 			if (!isOneHovered || !isRMouseDown)
 			{
